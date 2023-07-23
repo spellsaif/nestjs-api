d6a3db19f13b909e364c27ca81ec4e36a016623f
Md Saif Ali Ansari <71248588+spellsaif@users.noreply.github.com>
2023-07-23 11:43:49 +0000

    login added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import RegisterDTO from './dto/register.dto';
 import { compare, hash } from 'bcrypt';
+import LoginDTO from './dto/login.dto';
+import { jwtConfig } from 'src/config/jwt';
 
 @Injectable()
 export class AuthService {
@@ -30,5 +32,43 @@ export class AuthService {
                 message: 'user created successfully!!!'
             }
         }
+    }
+
+    async login(data: LoginDTO) {
+        const user = await this.prisma.user.findFirst({
+            where: {
+                email: data.email
+            }
+        })
+
+        if (!user) {
+            throw new HttpException('Invalid email or password', HttpStatus.BAD_REQUEST);
+        }
+
+        const comparePassword = await compare(data.password, user.password);
+
+        if (!comparePassword) {
+            throw new HttpException('Invalid email or password', HttpStatus.BAD_REQUEST);
+        }
+
+        const accessToken = this.generateJwt({
+            sub: user.id,
+            name: user.username,
+            email: user.email
+        })
+
+        return {
+            statusCode: 200,
+            message: 'logged in successfully.',
+            accessToken
+        }
+
+    }
+
+    generateJwt(payload: any) {
+        return this.jwt.sign(payload, {
+            secret: jwtConfig.secret,
+            expiresIn: jwtConfig.expired
+        })
     }
 }
