b2cb7bc28b4f92f7af34ae35e3e6ef9f4e64097d
Md Saif Ali Ansari <71248588+spellsaif@users.noreply.github.com>
2023-07-23 12:09:58 +0000

    added minor stuff

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, HttpCode, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import LoginDTO from "./dto/login.dto";
 import RegisterDTO from "./dto/register.dto";
@@ -14,6 +14,7 @@ export class AuthController {
     }
 
     @Post('login')
+    @HttpCode(200)
     login(@Body() loginDTO: LoginDTO) {
         return this.authService.login(loginDTO);
     }
