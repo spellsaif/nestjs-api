ef6f9032b416fc3ad22b2d644965a0a098d4e9a2
Md Saif Ali Ansari <71248588+spellsaif@users.noreply.github.com>
2023-07-24 13:45:13 +0000

    added: dream rest api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { DreamModule } from './dream/dream.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
-    PrismaModule],
+    PrismaModule,
+    DreamModule],
   controllers: [],
   providers: [],
 })
